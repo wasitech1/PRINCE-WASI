6e30761ea549af6b2c982779e1ae5ffab5deb210
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-30 10:00:43 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/tools-google.js
+++ plugins/tools-google.js
@@ -4,7 +4,7 @@ import googleIt from 'google-it'
 let handler = async (m, { conn, usedPrefix, command, args }) => {
   let full = /f$/i.test(command)
   let text = args.join` `
-  if (!text) return conn.reply(m.chat, `✳️ Que quieres buscar en Google?`, m)
+  if (!text) return conn.reply(m.chat, `✳️ What do you want to search on Google?`, m)
  m.react(rwait)
   let url = 'https://google.com/search?q=' + encodeURIComponent(text)
   let search = await googleIt({ query: text })
