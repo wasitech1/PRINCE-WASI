31a7c757ce01927cea11e53bfa2998788c8490d2
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-01 19:33:28 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -3,9 +3,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://github.com/Princegds786/PRINCE_BOT/raw/main/Assets/mp3/Audio5.mp3"
-    let url = "https://github.com/Guru322/GURU-BOT"
+    let url = "https://github.com/PRINCE-GDS/THE-PRINCE-BOT"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://i.imgur.com/jF3IodN.jpeg"
+    let img = "https://i.imgur.com/oX4ES3B.jpeg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -19,10 +19,10 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "I AM ALIVE",
+          title: "THE-PRINCE-BOT IS ALIVE",
           body: "PRINCE BOT",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/FFDazafm9Z5IYthaQecbyN',
+          sourceUrl: 'https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
