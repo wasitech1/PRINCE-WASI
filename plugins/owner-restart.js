7cc69bd61dcce31b192e90b626e506c3b4599b05
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-08 00:41:13 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,14 +2,14 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🔄 Reiniciando Bot...\n Espere un momento')
+    await m.reply('_*♻️Restarting Bot...\n Wait a moment*_')
     process.send('reset')
   } else throw 'eh'
 }
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['restart','reiniciar'] 
+handler.command = ['restart'] 
 
 handler.rowner = true
 
