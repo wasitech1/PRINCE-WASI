7183b4bc42520ba54cb11ae9cc226040fba1cde0
wasitech1 <161101271+wasitech1@users.noreply.github.com>
2024-03-03 14:02:41 +0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,12 +1,12 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = princeImg.getRandom()	
-const cat = `*_🛡️ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴍᴅ🛡️_*
+const cat = `*_🛡️𝐖𝐚𝐬𝐢 𝐌𝐃🛡️_*
 
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+*_—◉𝗗𝗲𝘃𝗲𝗹𝗽𝗼𝗿 wa.me/923192173398_*
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92_*
+*_—◉𝗗𝗘𝗩𝗘𝗟𝗣𝗢𝗥 2 wa.me/923192173398*
 *---------------------*
 
 
@@ -17,6 +17,6 @@ await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-handler.command = /^(owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
+handler.command = /^(owner|creator|propietario|info|dueña|propietaria|dueño|wasii|dev)$/i
 
 export default handler
